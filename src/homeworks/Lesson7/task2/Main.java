6bf0e7d37d1f2a98f41247debd6e3d2bd6e6d68f
Anton Novosad <noobito.2011@gmail.com>
2021-02-05 19:17:47 +0300

    add class Main

---
+++ src/homeworks/Lesson7/task2/Main.java
@@ -0,0 +1,19 @@
+package homeworks.Lesson7.task2;
+
+public class Main {
+    public static void main(String[] args) {
+        GasEngine gasEngine = new GasEngine(200);
+        DieselEngine dieselEngine = new DieselEngine(300, 2);
+
+        ManualGearBox manualGearBox = new ManualGearBox(5);
+        AutomaticGearBox automaticGearBox = new AutomaticGearBox(4, 'R');
+
+        Car car1 = new Car("Audi", 200, gasEngine, manualGearBox);
+        Car car2 = new Car("BMW", 300, dieselEngine, automaticGearBox);
+
+
+        car1.start();
+        System.out.println();
+        car2.start();
+    }
+}
