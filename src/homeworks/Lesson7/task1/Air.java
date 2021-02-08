7116b3f4d8b44ee34eb9796599ffd62603fb1850
Anton Novosad <noobito.2011@gmail.com>
2021-02-08 13:21:31 +0300

    add solution in class Air.java and Ground.java

---
+++ src/homeworks/Lesson7/task1/Air.java
@@ -0,0 +1,54 @@
+package homeworks.Lesson7.task1;
+
+public class Air extends Transport {
+    private int wingspan; //размах крыльев
+    private int minimumRunway;
+
+    public Air(String model, int power, int maxSpeed, int mass, int wingspan, int minimumRunway) {
+        super(model, power, maxSpeed, mass);
+        this.wingspan = wingspan;
+        this.minimumRunway = minimumRunway;
+    }
+
+    public Air() {
+    }
+
+    public int getWingspan() {
+        return wingspan;
+    }
+
+    public void setWingspan(int wingspan) {
+        this.wingspan = wingspan;
+    }
+
+    public int getMinimumRunway() {
+        return minimumRunway;
+    }
+
+    public void setMinimumRunway(int minimumRunway) {
+        this.minimumRunway = minimumRunway;
+    }
+
+    private String abilitiesAir() {
+        return "Размах крыльев: " + getWingspan() + ". Минимальная дальность(км): " + getMinimumRunway();
+    }
+
+    @Override
+    public void descriptionAir() {
+        super.descriptionAir();
+        System.out.println(abilitiesAir());
+    }
+
+    @Override
+    public void passengers(int passengers) {
+    }
+
+    @Override
+    public void useRocket() {
+    }
+
+    @Override
+    public void ejections() {
+
+    }
+}
---
+++ src/homeworks/Lesson7/task1/Ground.java
@@ -0,0 +1,46 @@
+package homeworks.Lesson7.task1;
+
+public class Ground extends Transport {
+    private int numberOfWheels;
+    private int fuelConsumption;
+
+    public Ground(String model, int power, int maxSpeed, int mass, int numberOfWheels, int fuelConsumption) {
+        super(model, power, maxSpeed, mass);
+        this.numberOfWheels = numberOfWheels;
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    public Ground(int numberOfWheels, int fuelConsumption) {
+        this.numberOfWheels = numberOfWheels;
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    public Ground() {
+    }
+
+    public int getNumberOfWheels() {
+        return numberOfWheels;
+    }
+
+    public int getFuelConsumption() {
+        return fuelConsumption;
+    }
+
+    private String abilitiesGround() {
+        return "Количество колес: " + getNumberOfWheels() + ". Расход топлива(л/100км): " + getFuelConsumption();
+    }
+
+    @Override
+    public void descriptionGround() {
+        super.descriptionGround();
+        System.out.println(abilitiesGround());
+    }
+
+    @Override
+    public void distance(double time) {
+    }
+
+    @Override
+    public void carrying(double kilogram) {
+    }
+}
