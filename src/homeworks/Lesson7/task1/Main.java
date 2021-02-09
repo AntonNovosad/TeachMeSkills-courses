038a38e4cdb91e145325d8345964ae944bce254e
Anton Novosad <noobito.2011@gmail.com>
2021-02-09 16:59:47 +0300

    change create objects passengerCar , AutoTruck , CivilPlane , WarPlane

--- src/homeworks/Lesson7/task1/Main.java
+++ src/homeworks/Lesson7/task1/Main.java
@@ -2,18 +2,18 @@ package homeworks.Lesson7.task1;
 
 public class Main {
     public static void main(String[] args) {
-        Transport audi = new PassengerCar("Audi", 22, 200, 2000, 4, 10, "Седан", 4);
-        Transport volvo = new AutoTruck("Volvo", 40, 100, 5000, 6, 20, 10000);
+        PassengerCar audi = new PassengerCar("Audi", 22, 200, 2000, 4, 10, "Седан", 4);
+        AutoTruck volvo = new AutoTruck("Volvo", 40, 100, 5000, 6, 20, 10000);
 
-        Transport plane = new CivilPlane("boing", 200, 700, 25000, 40,1000, 200,true);
-        Transport warPlane = new WarPlane("comet", 100, 1000, 15000, 20,2000, true,3);
+        CivilPlane boing = new CivilPlane("boing", 200, 700, 25000, 40,1000, 200,true);
+        WarPlane comet = new WarPlane("comet", 100, 1000, 15000, 20,2000, true,3);
 
-        audi.descriptionGround();
+        audi.description();
         System.out.println("Задание автомобиля:");
         audi.distance(2.5);
 
         System.out.println();
-        volvo.descriptionGround();
+        volvo.description();
         System.out.println("Задание грузовика: ");
         volvo.carrying(1000);
         volvo.carrying(2000);
@@ -22,24 +22,23 @@ public class Main {
 
 
         System.out.println();
-        plane.descriptionAir();
+        boing.description();
         System.out.println("Задание для пассажирского самолета:");
-        plane.passengers(100);
-        plane.passengers(100);
-        plane.passengers(1);
+        boing.passengers(100);
+        boing.passengers(100);
+        boing.passengers(1);
 
         System.out.println();
-        warPlane.descriptionAir();
+        comet.description();
 
-        warPlane.useRocket();
-        warPlane.useRocket();
-        warPlane.useRocket();
-        warPlane.useRocket();
+        comet.useRocket();
+        comet.useRocket();
+        comet.useRocket();
+        comet.useRocket();
 
         System.out.println();
-        warPlane.ejections();
-        warPlane.ejections();
-
+        comet.ejections();
+        comet.ejections();
     }
 }
 
