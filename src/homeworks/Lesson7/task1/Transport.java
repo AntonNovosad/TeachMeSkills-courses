be6d49537d971acdca9e1912c356d4ba4537dfdc
Anton Novosad <noobito.2011@gmail.com>
2021-02-09 17:01:02 +0300

    delete methods distance, carrying, passengers, useRocket, ejections, descriptionGround and descriptionsAir

--- src/homeworks/Lesson7/task1/Transport.java
+++ src/homeworks/Lesson7/task1/Transport.java
@@ -5,7 +5,6 @@ public class Transport {
     private int power;
     private int maxSpeed;
     private int mass;
-    private Air air;
 
     public Transport(String model, int power, int maxSpeed, int mass) {
         this.model = model;
@@ -54,33 +53,12 @@ public class Transport {
     }
 
     private String abilitiesTransport() {
-        return ". Мощность двигателя: " + getPower() +
+        return "Мощность двигателя: " + getPower() +
                 ". Мощность в киловаттах: " + powerInKilowatt(getPower()) +
                 ". Максимальная скорость(км/ч): " + getMaxSpeed() + ". Масса автомобиля: " + getMass();
     }
 
-    public void descriptionGround() {
-        System.out.println("Характеристики автомобиля: ");
-        System.out.println("Марка автомобиля: " + getModel() + abilitiesTransport());
-    }
-
-    public void descriptionAir() {
-        System.out.println("Характеристики самолета: ");
-        System.out.println("Модель самолета: " + getModel() + abilitiesTransport());
-    }
-
-    public void distance(double time) {
-    }
-
-    public void carrying(double kilogram) {
-    }
-
-    public void passengers(int passengers) {
-    }
-
-    public void useRocket() {
-    }
-
-    public void ejections() {
+    public void description(){
+        System.out.println(abilitiesTransport());
     }
 }
