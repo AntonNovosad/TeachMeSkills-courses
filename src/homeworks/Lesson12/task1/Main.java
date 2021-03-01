8999cf0d962989e32b0122f3c96200ff31e16d43
Anton Novosad <noobito.2011@gmail.com>
2021-03-01 13:24:32 +0300

    change path file in bufferedReader , add boolean check and if(check)

--- src/homeworks/Lesson12/task1/Main.java
+++ src/homeworks/Lesson12/task1/Main.java
@@ -15,23 +15,30 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("C:\\Users\\User\\IdeaProjects\\TeachMeSkills-courses\\src\\homeworks\\Lesson12\\task1\\text.txt"))) {
+        boolean check = true;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("src\\homeworks\\Lesson12\\task1\\text.txt"))) {
             String lineText;
             while ((lineText = bufferedReader.readLine()) != null) {
                 String[] mass = lineText.split(" ");
-                try (BufferedReader blackList = new BufferedReader(new FileReader("C:\\Users\\User\\IdeaProjects\\TeachMeSkills-courses\\src\\homeworks\\Lesson12\\task1\\blackList.txt"))) {
+                try (BufferedReader blackList = new BufferedReader(new FileReader("src\\homeworks\\Lesson12\\task1\\blackList.txt"))) {
                     String listBlackList;
                     while ((listBlackList = blackList.readLine()) != null) {
                         for (String s : mass) {
                             if (listBlackList.equalsIgnoreCase(s)) {
+                                check = false;
                                 System.out.println("Предложение не прошедшее цензуру: ");
                                 System.out.println(lineText);
+                                System.out.println("Слово: " + s);
+                                System.out.println();
                             }
                         }
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+            }
+            if (check) {
+                System.out.println("Текст прошел проверку на цензуру");
             }
         } catch (IOException e) {
             e.printStackTrace();
