078748215ee56958b3e52f8441c4b921c45f9c0a
Anton Novosad <noobito.2011@gmail.com>
2021-03-23 14:15:35 +0300

    moved in package hospital , change methods getName and getAge

---
+++ src/homeworks/Lesson16/hospital/Patient.java
@@ -0,0 +1,35 @@
+package homeworks.Lesson16.hospital;
+
+public class Patient {
+    private String name;
+    private int age;
+
+    public Patient(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Patient() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return name + ", возрастом " + age;
+    }
+}
