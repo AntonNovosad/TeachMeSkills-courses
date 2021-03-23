d6291efe5029e1c294e38d69aee4b7ac3b8e3ede
Anton Novosad <noobito.2011@gmail.com>
2021-03-23 16:29:46 +0300

    change solution

--- src/homeworks/Lesson16/Main.java
+++ src/homeworks/Lesson16/Main.java
@@ -1,18 +1,9 @@
 package homeworks.Lesson16;
 
-import static homeworks.Lesson16.QualificationLevel.*;
+import homeworks.Lesson16.hospital.Hospital;
 
 public class Main {
     public static void main(String[] args) {
-        Action action = new Action();
-        Registry registry = new Registry(action);
-        Doctor doctorOne = new Doctor("Ваня", INTERN, action);
-        Doctor doctorTwo = new Doctor("Александр", ORDINARYDOCTOR, action);
-        Doctor doctorThree = new Doctor("Денис", DEPARTMENTHEAD, action);
-
-        registry.start();
-        doctorOne.start();
-        doctorTwo.start();
-        doctorThree.start();
+        new Hospital().start();
     }
 }
