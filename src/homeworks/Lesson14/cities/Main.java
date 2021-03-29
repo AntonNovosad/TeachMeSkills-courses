81a1c7d2b225e68bf8f3a25ad06a4a1ef1f15a33
Anton Novosad <noobito.2011@gmail.com>
2021-03-29 19:27:31 +0300

    add solution

---
+++ src/homeworks/Lesson14/cities/Main.java
@@ -0,0 +1,9 @@
+package homeworks.Lesson14.cities;
+
+import homeworks.Lesson14.cities.entity.Game;
+
+public class Main {
+    public static void main(String[] args) {
+        new Game().start();
+    }
+}
